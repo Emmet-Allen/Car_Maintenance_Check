04799afef39c376dfad09260d0e7bd5ea4036471
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-10-31 06:17:31 -0400

    Added changes for up to 60k miles

---
+++ car_maintenance.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void carMaintence ()
+{
+  int miles;
+
+  cout << "Miles on Odometer Currently:";
+  cin >> miles;
+
+  while (cin.fail())
+    {
+      cin.clear();
+      cin.ignore();
+      cout << "Please enter a valid input" << endl;
+      cin >> miles;
+    }
+
+
+
+        if(miles < 30,000)
+        {
+          cout << "Change Air Filter" << endl; 
+            cout << "Change Fuel Filter" << endl; 
+        }
+
+        else if (miles > 30,000 && miles < 60,000){
+          cout << "Check Battery" << endl;
+           cout << "Check Break Fluid" << endl;
+            cout << "Check Break Pads" << endl; 
+             cout << "Check Break Rotors" << endl;
+              cout << "Change Tranmission Fluid" << endl; 
+                 
+        }
+    
+
+}
+
+int main ()
+{
+  system("color 0A");
+  carMaintence ();
+
+  return 0;
+}
