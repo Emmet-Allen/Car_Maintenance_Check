55e984849decd7fa90d70953d582a790957c0601
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-10-31 06:57:01 -0400

    Formatted Code

--- car_maintenance.cpp
+++ car_maintenance.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
+
 #include <windows.h>
+
+#include <string>
 
 using namespace std;
 
-void carMaintence ()
+void carMaintence()
 {
-  int miles;
+    int miles;
+    system("color 0A");
 
-  cout << "Miles on Odometer Currently:";
-  cin >> miles;
+    cout << "Miles on Odometer Currently:";
+    cin >> miles;
 
-  while (cin.fail())
+    while (cin.fail())
     {
-      cin.clear();
-      cin.ignore();
-      cout << "Please enter a valid input" << endl;
-      cin >> miles;
+        cin.clear();
+        cin.ignore();
+        cout << "Please enter a valid input" << endl;
+        cin >> miles;
     }
 
-if (miles == 0 || miles > 30,000){
-  cout << "The car is pretty new, continue general maintance" << endl; 
+    if (miles == 0 || miles > 30000)
+    {
+        cout << "The car is pretty new, continue general maintance" << endl;
+        system("pause");
+    }
+    else if (miles < 30000)
+    {
+        cout << "Change Air Filter" << endl;
+        cout << "Change Fuel Filter" << endl;
+        system("pause");
+    }
+    else if (miles > 30000 && miles < 60000)
+    {
+        cout << "Check Battery" << endl;
+        cout << "Check Break Fluid" << endl;
+        cout << "Check Break Pads" << endl;
+        cout << "Check Break Rotors" << endl;
+        cout << "Replace Tranmission Fluid" << endl;
+        system("pause");
+    }
+    else if (miles > 60000 && miles < 90000)
+    {
+        cout << "Change Car Hoses" << endl;
+        cout << "Replace Power Steering Fluid" << endl;
+        cout << "Replace Timing Belt" << endl;
+        system("pause");
+    }
 }
 
-       else if(miles < 30,000)
-        {
-          cout << "Change Air Filter" << endl; 
-            cout << "Change Fuel Filter" << endl; 
-        }
-
-        else if (miles > 30,000 && miles < 60,000){
-          cout << "Check Battery" << endl;
-           cout << "Check Break Fluid" << endl;
-            cout << "Check Break Pads" << endl; 
-             cout << "Check Break Rotors" << endl;
-              cout << "Replace Tranmission Fluid" << endl; 
-                 
-        }
-
-        else if(miles > 60,000 && miles < 90,000){
-          cout << "Change Car Hoses" << endl;
-          cout << "Replace Power Steering Fluid" << endl;
-          cout << "Replace Timing Belt" << endl;
-        }
-    
-
-}
-
-int main ()
+int main()
 {
-  system("color 0A");
-  system("pause");
-  carMaintence ();
-  
-
-
-  return 0;
+    carMaintence();
+    return 0;
 }
